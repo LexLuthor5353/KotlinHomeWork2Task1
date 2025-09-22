f4e8ac5c534f446459af191283b033f93ec4db95
Aleksandr Karkhunen <ajlvatanabe@gmail.com>
2025-09-22 13:33:01 +0300

    Переписана программа целиком, так как была не корректно сформирована логика подсчета вчеремени

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,32 @@
 fun main() {
-    val seconds:Int = 240
+    val seconds: Int = 660
 
-    val minute:Int = seconds/60
-    val secInMin = minute % 10
+    val totalMinut = seconds / 60
+    val totalHours = totalMinut / 60
 
-    val hours:Int = minute/60
-    val minInHours = hours % 10
+    val minutesOstatok = totalMinut % 60
+    val hoursOstatok = totalHours % 24
 
-
-    var agoToText = when {
-        (seconds > 0 && seconds <= 60) -> {println("«был(а) только что»")}
-        (seconds > 60 && seconds < 60 * 60 && secInMin == 1) -> {println("«был(а) $minute минуту назад»")}
-        (seconds > 60 && seconds < 60 * 60 && secInMin in 2..4) -> {println("«был(а) $minute минуты назад»")}
-        (seconds > 60 && seconds < 60 * 60 && minute % 10 == 5 || minute % 10 > 5 || minute % 10 == 0 && minute < 60) -> {println("«был(а) $minute минут назад»")}
-
-        (seconds > 60 * 60 && seconds < 24 * 60 * 60 && minInHours == 1) -> {println("«был(а) $hours час назад»")}
-        (seconds > 60 * 60 && seconds < 24 * 60 * 60 && minInHours in 2..4) -> {println("«был(а) $hours часа назад»")}
-        (seconds > 60 * 60 && seconds < 24 * 60 * 60 && minInHours == 0) -> {println("«был(а) $hours часов назад»")}
-
-        (seconds > 24 * 60 * 60 && seconds < 48 * 60 * 60) -> {println("«был(а) вчера»")}
-        (seconds > 48 * 60 * 60 && seconds < 72 * 60 * 60) -> {println("«был(а) позавчера»" )}
-        else -> {println("«был(а) давно»")}
+    val minutText = when {
+        totalMinut % 10 == 1 && totalMinut % 100 != 11 -> "минуту"
+        totalMinut % 10 in 2..4 && totalMinut % 100 !in 12..14 -> "минуты"
+        else -> "минут"
     }
 
+    val hourText = when {
+        totalHours % 10 == 1 && totalHours % 100 != 11 -> "час"
+        totalHours % 10 in 2..4 && totalHours % 100 !in 12..14 -> "часа"
+        else -> "часов"
+    }
 
+    val result = when {
+        seconds <= 60 -> "только что"
+        seconds < 3600 -> "$totalMinut $minutText назад"
+        seconds < 86400 -> "$totalHours $hourText назад"
+        seconds < 172800 -> "вчера"
+        seconds < 259200 -> "позавчера"
+        else -> "давно"
+    }
 
+    println("был(а) $result")
 }
